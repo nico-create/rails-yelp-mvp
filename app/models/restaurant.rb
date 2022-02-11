83183e6192da2eb6c7aa051541e0bcf5657203fe
Nico L <l.nico08@laposte.net>
2022-02-12 08:58:31 +0900

    adding review model, still need to correct restaurant destroy and rating should be an integer parts

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 10 }
+  validates :address, presence: true, length: { minimum: 10 }
+  validates :category, presence: true, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
+  has_many :reviews
 end
