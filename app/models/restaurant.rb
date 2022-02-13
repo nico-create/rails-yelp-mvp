c18a43e7b27c68a560e1b4b8b29e05e4bf81a32e
Nico L <l.nico08@laposte.net>
2022-02-13 12:27:49 +0900

    finished validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 10 }
+  validates :name, presence: true, length: { minimum: 5 }
   validates :address, presence: true, length: { minimum: 10 }
   validates :category, presence: true, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
