984381108bbbc4434439b335c43d3eadfa37e485
Nico L <l.nico08@laposte.net>
2022-02-13 17:24:13 +0900

    minor improvements

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 5 }
-  validates :address, presence: true, length: { minimum: 10 }
+  validates :name, presence: true, length: { minimum: 5, maximum: 30 }
+  validates :address, presence: true, length: { minimum: 10, maximum: 65 }
   validates :category, presence: true, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
   has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :content, presence: true, length: { minimum: 3 }
+  validates :content, presence: true, length: { minimum: 3, maximum: 100 }
   validates :rating, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
   belongs_to :restaurant
 end
