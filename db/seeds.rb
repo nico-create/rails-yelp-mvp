87f9d6ef1ca1a2ed53ab424928eaa5535efeb084
Nico L <l.nico08@laposte.net>
2022-02-13 14:12:09 +0900

    adding restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
+puts 'cleaning up database'
+Restaurant.destroy_all
+puts 'database is clean'
+
+puts 'creating restaurants'
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "restaurant #{restaurant.id} is created"
+end
+
+puts "done"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
